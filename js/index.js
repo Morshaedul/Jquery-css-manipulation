9de6d88cae43e4e0f484842e8c80a6587712af6b
Morshaedul <morshaedulanirbanit@gmail.com>
2022-03-03 20:36:44 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.querySelector('h1').classList.add("para1")
+
+// $('h1').css({'color':'red',
+//             'font-style':'bold',
+//             "background-color": "black",
+//             "font-size": "60px"
+
+// });
